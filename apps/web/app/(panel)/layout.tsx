32e0588e7971bf3850ede08e47e8a4ecedac572f
riddhesh-mahajan <riddhesh.maha@gmail.com>
2023-10-25 23:09:38 +0530

    added logout

--- apps/web/app/(panel)/layout.tsx
+++ apps/web/app/(panel)/layout.tsx
@@ -11,6 +11,7 @@ export default function DashboardLayout({
 }) {
   const pathname = usePathname();
   const [selectedTab, setselectedTab] = useState("dashboard");
+  const [showProfileMenu, setshowProfileMenu] = useState(false);
 
   useEffect(() => {
     console.log(pathname);
@@ -25,8 +26,13 @@ export default function DashboardLayout({
   }, [pathname]);
 
   return (
-    <section className="p-8">
-      <div className="flex justify-between align-middle mb-6">
+    <section
+      className="h-screen p-8"
+      onClick={() => {
+        setshowProfileMenu(false);
+      }}
+    >
+      <div className="flex justify-between mb-6 align-middle">
         {/* Title */}
         <h1 className="text-3xl font-bold leading-tight text-center">
           <span className="text-blue-600">/</span>Slash
@@ -60,9 +66,47 @@ export default function DashboardLayout({
         </div>
 
         {/* Profile Picture */}
-        <div className="flex align-middle ps-14">
-          <div className="w-12 h-12 rounded-full text-white flex items-center justify-center border-white border-2">
-            <span className="text-2xl font-semibold">J</span>
+        <div
+          className="flex align-middle cursor-pointer ps-14"
+          onClick={(e) => {
+            e.stopPropagation();
+            setshowProfileMenu(!showProfileMenu);
+          }}
+        >
+          <div className="flex items-center justify-center w-12 h-12 text-white border-2 border-white rounded-full">
+            <span className="text-2xl font-semibold select-none">J</span>
+          </div>
+        </div>
+
+        <div
+          onClick={(e) => {
+            e.stopPropagation();
+            setshowProfileMenu(!showProfileMenu);
+          }}
+          className={`absolute right-0 z-10 w-56 mt-14 me-10 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${
+            showProfileMenu
+              ? "transition ease-out duration-10 transform opacity-100 scale-100"
+              : "transition ease-in duration-75 transform opacity-0 scale-95"
+          }`}
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="menu-button"
+          tabIndex={-1}
+        >
+          <div className="py-1" role="none">
+            {/* Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" */}
+            <a
+              href={`${process.env.NEXT_PUBLIC_LANDINGG_BASE_URL}/login`}
+              onClick={() => {
+                localStorage.clear();
+              }}
+              className="block px-4 py-2 text-sm text-gray-700"
+              role="menuitem"
+              tabIndex={-1}
+              id="menu-item-0"
+            >
+              Logout
+            </a>
           </div>
         </div>
       </div>
